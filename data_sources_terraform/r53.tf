4f5fcff3d46b853c580640a935f909c8a6c862db
muneershaik <muneer509@gmail.com>
2025-04-11 20:12:59 +0530

    data_source

---
+++ data_sources_terraform/r53.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "expense_r53" {
+  count = length(var.instance_names)
+  zone_id = var.zone_id
+  type = "A"
+  ttl = 1
+  name = "${var.instance_names[count.index]}.${var.domain_name}"
+    
+  records = [aws_instance.terraform_instance[count.index].private_ip]
+
+
+}
