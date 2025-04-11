df2a6d0743ba1151603bd5336c4a8aadbe2df073
muneershaik <muneer509@gmail.com>
2025-04-11 20:47:36 +0530

    for_practice

---
+++ for_practice/ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "terraform_instance" {
+
+    ami = data.aws_ami.joindevops.id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = merge(
+        var.common_tags,
+        {
+            Name = "For_practice_terraform"
+        }
+    )
+}
+
+resource "aws_security_group" "allow_ssh_terraform" {
+    name        = var.sg_name
+    description = "Allow port number 22 for SSH access"
+
+    # usually we allow everything in egress
+    egress {
+        from_port        = var.egress_fronmandto_port
+        to_port          = var.egress_fronmandto_port
+        protocol         = var.protocol
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port        = var.ingress_fromandto_port
+        to_port          = var.ingress_fromandto_port
+        protocol         = var.protocol
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "allow_ssh_terraform"
+    }
+}
